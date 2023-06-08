a15c8f05418a5763b9ab4b190d7aea4c135abc07
avnish kumar <11avnishkumar@gmail.com>
2023-06-08 07:42:21 +0530

    fixes ui

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
     <div className="w-full min-h-screen bg-gray-200 px-2 py-2">
       <Search/>
       <div className="mt-3 grid grid-cols-1 md:grid-cols-3 gap-x-4">
-        <div className="mb-4 order-last md:mb-0 md:col-span-2 overflow-hidden rounded-md">
+        <div className="order-last md:mb-0 md:col-span-2 overflow-hidden rounded-md">
           {/* For displaying world map */}
           <WorldMap/>
         </div>
-        <div className="">
+        <div className="mb-4 md:mb-0">
           {/* display country */}
           <Country />
         </div>
